b7846521b5af9041c76cc4dd68542c43633b52c9
Paul Emmanuel Chukwuma <chukspaul777@gmail.com>
2026-04-22 21:59:36 +0100

    Remove server listen log and export app

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -48,6 +48,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
